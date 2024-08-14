040fb290e14fd1533f39f539da2929d10cccd056
unknown <fined000@naver.com>
2024-08-14 10:56:03 +0900

    main 브랜치 dm 충돌 해결

--- services/ggi-web/app/dm/components/layout/ChaProvider.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { ChakraProvider } from '@chakra-ui/react'
-
-export default function ChaProvider({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
-}
