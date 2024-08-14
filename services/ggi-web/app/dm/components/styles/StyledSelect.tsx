233cb861739f476aaeb9184cb9b19ef24e7672d9
unknown <fined000@naver.com>
2024-08-14 09:57:55 +0900

    dm 충돌 해결

--- services/ggi-web/app/dm/components/styles/StyledSelect.tsx
+++ services/ggi-web/app/dm/components/styles/StyledSelect.tsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e5b7f72b62cb4705a6d53727430016d0e20475b
 import { Select } from "@chakra-ui/react"
 import { theme } from "./theme"
 import { UpDownIcon } from "@chakra-ui/icons"
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e5b7f72b62cb4705a6d53727430016d0e20475b
 interface ISelectProps {
   options: Array<{
     value?: string,
@@ -26,25 +18,6 @@ export default function StyledSelect({
 }: ISelectProps) {
 
   return (
-<<<<<<< HEAD
-    <Select 
-      size={'lg'} 
-      minWidth={'190px'}
-      width={wider ? '326.5px' :'auto'}
-      maxWidth={'335px'}
-      height={'50px'}
-      border={`1px solid ${theme.palette.graySecondary}`}
-      borderRadius={'16px'}
-      marginRight={noMargin ? `0` :`5px`}
-      icon={<UpDownIcon fontSize={'12px'} color={`${theme.palette.grayMain}`} />}
-    >
-      {options.length && options.map((option, idx) => (
-        <option key={idx} value={option.value}>
-          {option.name}
-        </option>
-      ))}
-    </Select>
-=======
     // <Select 
     //   size={'lg'} 
     //   minWidth={'190px'}
@@ -63,6 +36,5 @@ export default function StyledSelect({
     //   ))}
     // </Select>
     <div></div>
->>>>>>> 0e5b7f72b62cb4705a6d53727430016d0e20475b
   )
 }
