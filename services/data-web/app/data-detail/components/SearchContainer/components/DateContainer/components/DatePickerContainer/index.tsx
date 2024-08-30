01a75a01a47b807e7898879862d000806adbbadb
JeongTaekCho <cjt3591@gmail.com>
2024-08-30 14:41:33 +0900

    Design: 기준일자 버튼 컴포넌트 제작

--- services/data-web/app/data-detail/components/SearchContainer/components/DateContainer/components/DatePickerContainer/index.tsx
+++ services/data-web/app/data-detail/components/SearchContainer/components/DateContainer/components/DatePickerContainer/index.tsx
@@ -1,66 +1,34 @@
 import CalendarIcon from '@/app/data-detail/components/SearchContainer/components/DateContainer/components/DatePickerContainer/components/CalendarIcon'
 import DatePickerComponent from '@/app/shared/components/inputs/DatePickerComponent'
-import { formatDate } from '@/app/shared/utils/formatDate'
-import { useState } from 'react'
-import { Control, Controller, useFormContext } from 'react-hook-form'
+import { Control, Controller } from 'react-hook-form'
 
 interface DatePickerContainerProps {
   control: Control<any>
+  isOpenStartPicker: boolean
+  isOpenEndPicker: boolean
+  startDateValue: string
+  endDateValue: string
+  handleClickStartPicker: () => void
+  handleClickEndPicker: () => void
+  handleChangeStartDate: (value: Date) => void
+  handleChangeEndDate: (value: Date) => void
+  handleStartDateApply: () => void
+  handleEndDateApply: () => void
 }
 
 export default function DatePickerContainer({
   control,
+  isOpenStartPicker,
+  isOpenEndPicker,
+  startDateValue,
+  endDateValue,
+  handleClickStartPicker,
+  handleClickEndPicker,
+  handleChangeStartDate,
+  handleChangeEndDate,
+  handleStartDateApply,
+  handleEndDateApply,
 }: DatePickerContainerProps) {
-  const [isOpenStartPicker, setIsOpenStartPicker] = useState(false)
-  const [isOpenEndPicker, setIsOpenEndPicker] = useState(false)
-  const [startDateValue, setStartDateValue] = useState('yyyy / mm / dd')
-  const [endDateValue, setEndDateValue] = useState('yyyy / mm / dd')
-  const [startDate, setStartDate] = useState(null)
-  const [endDate, setEndDate] = useState(null)
-
-  const { setValue } = useFormContext()
-
-  const handleClickStartPicker = () => {
-    setIsOpenStartPicker((prev) => !prev)
-  }
-  const handleClickEndPicker = () => {
-    setIsOpenEndPicker((prev) => !prev)
-  }
-
-  const handleChangeStartDate = (value) => {
-    setStartDate(value)
-  }
-
-  const handleChangeEndDate = (value) => {
-    setEndDate(value)
-  }
-
-  const handleStartDateApply = () => {
-    if (endDate && startDate > endDate) {
-      alert('시작 날짜는 종료 날짜보다 빠를 수 없습니다.')
-      return
-    }
-
-    if (!endDate || startDate <= endDate) {
-      setValue('startDate', startDate)
-      setStartDateValue(formatDate(startDate))
-      setIsOpenStartPicker(false)
-    }
-  }
-
-  const handleEndDateApply = () => {
-    if (startDate && endDate < startDate) {
-      alert('종료 날짜는 시작 날짜보다 빠를 수 없습니다.')
-      return
-    }
-
-    if (!startDate || endDate >= startDate) {
-      setValue('endDate', endDate)
-      setEndDateValue(formatDate(endDate))
-      setIsOpenEndPicker(false)
-    }
-  }
-
   return (
     <div className="w-[852px] h-[50px] px-4 border border-[#E5E7EB] rounded-2xl flex items-center gap-2">
       <CalendarIcon />
