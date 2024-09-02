f4f5f17ca41da0261889b218a40735f7b6f5038a
JeongTaekCho <cjt3591@gmail.com>
2024-09-02 10:04:40 +0900

    Modify: 버튼 공통 컴포넌트 수정

--- services/data-web/app/shared/components/buttons/Button/index.tsx
+++ services/data-web/app/shared/components/buttons/Button/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonHTMLAttributes } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode
+  text: string
   width?: string
   height?: string
   onClick?: () => void
@@ -10,42 +10,33 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button = ({
-  children,
-  width = 'w-[166px]',
-  height = 'h-[50px]',
+  text,
+  width,
+  height,
   onClick,
   className = '',
   styleType,
   ...props
 }: ButtonProps) => {
   const backgroundColor =
-    styleType === 'colored'
-      ? 'bg-[#2563EB]'
-      : styleType === 'plain'
-        ? 'bg-[#F3F4F6]'
-        : ''
+    styleType === 'colored' ? 'bg-[#2563EB]' : 'bg-[#F3F4F6]'
 
-  const textColor =
-    styleType === 'colored'
-      ? 'text-[#fff]'
-      : styleType === 'plain'
-        ? 'text-[#6B7280]'
-        : ''
+  const textColor = styleType === 'colored' ? 'text-[#fff]' : 'text-[#6B7280]'
 
   const borderColor =
-    styleType === 'colored'
-      ? 'border-[#2563EB]'
-      : styleType === 'plain'
-        ? 'border-[#E5E7EB]'
-        : ''
+    styleType === 'colored' ? 'border-[#2563EB]' : 'border-[#E5E7EB]'
 
   return (
     <button
       onClick={onClick}
-      className={`${width} ${height} ${backgroundColor} ${textColor} font-bold rounded-lg border ${borderColor} ${className} cursor-pointer`}
+      className={`${backgroundColor} ${textColor} font-bold rounded-lg border ${borderColor} ${className}`}
+      style={{
+        width: width || '166px',
+        height: height || '50px',
+      }}
       {...props}
     >
-      {children}
+      {text}
     </button>
   )
 }
