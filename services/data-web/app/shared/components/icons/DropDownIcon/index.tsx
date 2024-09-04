bd565beeae80e310cf0896738b77fd46bd5556db
JeongTaekCho <cjt3591@gmail.com>
2024-09-04 09:55:48 +0900

    Design: 헤더 반응형 퍼블리싱

---
+++ services/data-web/app/shared/components/icons/DropDownIcon/index.tsx
@@ -0,0 +1,19 @@
+export default function DropDownIcon() {
+  return (
+    <svg
+      width="18"
+      height="10"
+      viewBox="0 0 18 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17 1L9 9L1 1"
+        stroke="#1F2937"
+        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
