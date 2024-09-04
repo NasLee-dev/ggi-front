13f38e8470f998776a30f8bbf947cb82fdee8c5f
JeongTaekCho <cjt3591@gmail.com>
2024-09-04 15:12:31 +0900

    Modify: 통계 다운로드 버튼 텍스트 수정

--- services/data-web/app/data-detail/components/AreaDetailContainer/components/DownloadBtn/index.tsx
+++ services/data-web/app/data-detail/components/AreaDetailContainer/components/DownloadBtn/index.tsx
@@ -2,8 +2,8 @@ import DownloadIcon from '@/app/data-detail/components/AreaDetailContainer/compo
 
 export default function DownloadBtn() {
   return (
-    <button className="w-[111px] h-[50px] border border-[#E5E7EB] rounded-2xl flex justify-center gap-[12px] items-center ggi:w-full ggi:mt-5">
-      <span className="text-[16px] font-bold text-[#6B7280]">다운로드</span>
+    <button className="w-[100px] h-[50px] border border-[#E5E7EB] rounded-2xl flex justify-center gap-[12px] items-center ggi:w-full ggi:mt-5">
+      <span className="text-[16px] font-bold text-[#6B7280]">EXCEL</span>
       <DownloadIcon />
     </button>
   )
